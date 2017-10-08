3faccd29d6c5ff89db4b5815f43cc95da6e6153f
Vbobell <vinicius.bobel41@gmail.com>
2017-10-08 18:04:34 -0300

    Modify structure

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
+const pool = require('./lib/db');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
@@ -13,6 +13,43 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/meta', function(request, response) {
+  response.render('pages/meta');
+});
+
+app.get('/prestadoresServico', function(request, response){
+  pool.connect( function(err, client, done) {
+
+  if (err) {
+    return console.error('error fetching client from pool', err);
+  }
+
+  var querySearch = 'SELECT prestador.imagem, prestador.nome, prestador.profissao,';
+      querySearch += 'endereco.cidade, endereco.bairro, endereco.endereco,';
+      querySearch += 'contato.dddT, contato.telefone, contato.dddW, contato.whats, contato.email ';
+      querySearch += 'FROM prestador, endereco, contato ';
+      querySearch += 'WHERE prestador.idPrestador = endereco.idPrestador ';
+      querySearch += 'AND prestador.idPrestador = contato.idPrestador';
+
+  client.query(querySearch, function(err, result) {
+    done();
+    if (err) {
+      return console.error('error running query', err);
+    }
+    response.json({"prestadorServico" : result.rows});
+  });
+
+});
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+
+ 
+
+
+
+
