896c8b1f6d87de3b6bdff059609a632446908206
Vbobell <vinicius.bobel41@gmail.com>
2017-10-09 23:16:23 -0300

    Create archive database

---
+++ models/base.sql
@@ -0,0 +1,8 @@
+create table admin(
+	id_admin serial NOT NULL primary key,  
+    name_admin varchar(30) DEFAULT NULL, 
+    registre varchar(10) DEFAULT NULL,
+    user_level smallint DEFAULT NULL
+);
+
+
