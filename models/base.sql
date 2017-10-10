512b9507bd4874366607cbf75ede1c1ac25b1ebd
Vbobell <vinicius.bobel41@gmail.com>
2017-10-09 23:33:35 -0300

    Add new column password

--- models/base.sql
+++ models/base.sql
@@ -2,7 +2,8 @@ create table admin(
 	id_admin serial NOT NULL primary key,  
     name_admin varchar(30) DEFAULT NULL, 
     registre varchar(10) DEFAULT NULL,
-    user_level smallint DEFAULT NULL
+    user_level smallint DEFAULT NULL,
+    password varchar(30) DEFAULT 123456
 );
 
 
