9ed2251ae1ce725c12b06d8070eee5210752c63e
Vbobell <vinicius.bobel41@gmail.com>
2017-10-18 00:50:26 -0200

    test insert data admin

--- models/crud/generic-crud.js
+++ models/crud/generic-crud.js
@@ -20,8 +20,7 @@ class Crud {
             if (err) {
                 return console.error('error fetching client from pool', err);
             }
-            values.forEach(function (i) {
-                console.log(values[i]);
+            for(var i = 0; i < values.length; i++){
                 client.query('insert into ' + table + ' ' + columns + ' values ($1, $2, $3) ', values[i], function (err, result) {
                     done();
                     if (err) {
@@ -29,7 +28,7 @@ class Crud {
                     }
                     return json(result.affectedRows);
                 });
-            });
+            }
         });
     }
 }
