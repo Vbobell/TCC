3847538b59918528c4162f6bb2391b1189f8e770
Vbobell <vinicius.bobel41@gmail.com>
2017-10-21 17:49:18 -0200

    test upload file bucket

--- index.js
+++ index.js
@@ -37,24 +37,18 @@ app.get('/view/importAdmin', function(request, response){
 app.post('/upload', (req, res) => {
   const s3 = new aws.S3();
   const fileName = req.files.name;
-  const fileType = req.files.mimetype;
+  const file = req.files;
   const s3Params = {
     Bucket: S3_BUCKET,
     Key: fileName,
-    Expires: 60,
-    ContentType: fileType,
-    ACL: 'public-read'
+    Body: file
   };
 
-  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+  s3.upload(s3Params, (err, data) => {
     if(err){
       console.log(err);
       return res.end();
     }
-    const returnData = {
-      signedRequest: data,
-      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
-    };
     res.end();
   });
 });
