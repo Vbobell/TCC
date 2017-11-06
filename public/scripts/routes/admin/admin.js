ab7ce2ead9f4232a9220918ffaf828b9fb4906f8
Vbobell <vinicius.bobel41@gmail.com>
2017-11-06 21:49:21 -0200

    function created for call import admin

---
+++ public/scripts/routes/admin/admin.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.menu-buttom').on('click', function(){
+
+    });
+
+    $('.item').on('click', function(){
+        var url = $(this).attr('data-item') + '/' + 
+        $(this).parent().attr('data-content') + '/?data=' + $(this).attr('data-item');
+        $.ajax({
+            url : url,
+            contentType: 'html',
+            type: 'GET'
+        }).done(function(data){
+            $('body').append($(data)[1]);
+            $('body').append($(data)[3]);
+        });
+    });
+});
