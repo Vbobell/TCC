cc1080e2087d0e05e78dc87b23edabb77c385db7
Vbobell <vinicius.bobel41@gmail.com>
2017-11-10 00:15:52 -0200

    define variable valid login.

--- controller/manage/manage-admin.js
+++ controller/manage/manage-admin.js
@@ -12,8 +12,7 @@ class ManageAdmin{
     loginValidation(query, callback){
         let crudAdmin = new CrudAdmin();
         crudAdmin.selectUser(query.user,query.password, data => {
-            let valid = JSON.parse(data);
-            if(valid[0].count == 1)
+            if(JSON.parse(data)[0].count == 1)
                 return callback(true);
             else
                 return callback(false);
