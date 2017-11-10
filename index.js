09edc852f6b38a1f2d454038ce968b6f4044e9c4
Vbobell <vinicius.bobel41@gmail.com>
2017-11-09 23:10:15 -0200

    Add session validation

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var app = express();
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var fileUpload = require('express-fileupload');
-var ControllerImport = require('./controller/importFile/controller-import');
-var Route = require('./controller/viewRoutes/routes');
-var ManageAdmin = require('./controller/manage/manage-admin');
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const ControllerImport = require('./controller/importFile/controller-import');
+const Route = require('./controller/viewRoutes/routes');
+const ManageAdmin = require('./controller/manage/manage-admin');
 
 app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
@@ -38,37 +38,39 @@ app.get('/admin', function (request, response) {
 });
 
 app.get('/admin/import/*', function (request, response) {
-  var route = new Route(app.get('views') + '/pages' + request.path, request.query);
+  if (request.session.user){  
+  let route = new Route(app.get('views') + '/pages' + request.path, request.query);
   route.getRoute('.ejs', data => {
     if (data)
       response.render('pages'+ request.path + request.query.data);
     else
       response.redirect('pages/error');
   });
+  }else
+    response.redirect('/');
 });
 
 app.get('/admin/select/*', function (request, response) {
-  var route = new Route(app.get('controller') + '/manage' + request.path, request.query);
-  route.getRoute('.ejs', data => {
-    if (data){
-      response.write(JSON.stringify(data));
-    }
-    else
-      response.render('pages/error');
-  });
+  if (request.session.user){
+    let route = new Route(app.get('controller') + '/manage' + request.path, request.query);
+    route.getRoute('.ejs', data => {
+      if (data){
+        response.write(JSON.stringify(data));
+      }else
+        response.redirect('pages/error');
+    });
+  }else
+    response.redirect('/');
+});
+
+app.get('/error', function (request, response){
+  response.render('pages/error');
 });
 
 app.post('/login', function(request, response){
-  var manageAdmin = new ManageAdmin();
+  let manageAdmin = new ManageAdmin();
   manageAdmin.loginValidation(request.body, valid => {
     if(valid){
-      request.session.user = request.body;
-      let options = {
-        maxAge: request.session.cookie.maxAge,
-        httpOnly: true,
-        signed: true
-    }
-      res.cookie('login', request.body.user, options);
       request.session.user = request.body.user;
       response.redirect('pages/admin');
       response.end();
@@ -78,11 +80,14 @@ app.post('/login', function(request, response){
 });
 
 app.post('/import/*', (request, response) => {
-  var controller = new ControllerImport(request.params[0], request.files.csv.data.toString('utf8').split('\r\n'));
-  controller.csvInsertData(callback => {
-    response.write(JSON.stringify(callback));
-    response.end();
-  });
+  if (request.session.user){
+    let controller = new ControllerImport(request.params[0], request.files.csv.data.toString('utf8').split('\r\n'));
+    controller.csvInsertData(callback => {
+      response.write(JSON.stringify(callback));
+      response.end();
+    });
+  }else
+    response.redirect('/');
 });
 
 app.listen(app.get('port'), function () {
