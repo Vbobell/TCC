bee818a19c78d80021229872adc81ece636bc3a4
Vbobell <vinicius.bobel41@gmail.com>
2017-11-10 00:02:10 -0200

    test log.

--- controller/manage/manage-admin.js
+++ controller/manage/manage-admin.js
@@ -11,8 +11,11 @@ class ManageAdmin{
     }
     loginValidation(query, callback){
         var crudAdmin = new CrudAdmin();
-        crudAdmin.selectUser(query.user,query.password, (data) => {
+        crudAdmin.selectUser(query.user,query.password, data => {
             var valid = JSON.parse(data);
+            console.log(valid);
+            console.log(valid.count);
+            console.log(valid.count == 1);
             if(valid.count == 1)
                 return callback(true);
             else
