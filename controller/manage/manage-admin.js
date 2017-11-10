a4db8a8ba527ed57dae92c8023e87caa6b252dd3
Vbobell <vinicius.bobel41@gmail.com>
2017-11-09 23:46:39 -0200

    test get data user.

--- controller/manage/manage-admin.js
+++ controller/manage/manage-admin.js
@@ -12,6 +12,7 @@ class ManageAdmin{
     loginValidation(query, callback){
         var crudAdmin = new CrudAdmin();
         crudAdmin.selectUser(query.user,query.password, (data) => {
+            console.log(data);
             if(data.count == 1)
                 return callback(true);
             else
