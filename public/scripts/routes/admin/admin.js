50b3f72c5beb5ecac22b56b6607bb6a2630fdc32
Vbobell <vinicius.bobel41@gmail.com>
2017-11-11 16:06:57 -0200

    Created method for content show and hide

--- public/scripts/routes/admin/admin.js
+++ public/scripts/routes/admin/admin.js
@@ -4,18 +4,19 @@ $(document).ready(function(){
     });
 
     $('.item').on('click', function(){
-        $('.content').fadeOut(2000);
-        $('[data-content="generic"]').remove();
-        var url = $(this).attr('data-item') + '/' + 
-        $(this).parent().attr('data-content') + '/?data=' + $(this).attr('data-item');
-        $.ajax({
-            url : url,
-            dataType : 'html',
-            type: 'GET'
-        }).done(function(data){
-            $('body').append($(data)[1]);
-            $('body').append($(data)[3]);
-            $('.content[data-content="generic"]').fadeIn(2000);
+        $('.content').fadeOut(2000, function(){
+            $('[data-content="generic"]').remove();
+            var url = $(this).attr('data-item') + '/' + 
+            $(this).parent().attr('data-content') + '/?data=' + $(this).attr('data-item');
+            $.ajax({
+                url : url,
+                dataType : 'html',
+                type: 'GET'
+            }).done(function(data){
+                $('body').append($(data)[1]);
+                $('body').append($(data)[3]);
+                $('.content[data-content="generic"]').fadeIn(2000);
+            });
         });
     });
 });
