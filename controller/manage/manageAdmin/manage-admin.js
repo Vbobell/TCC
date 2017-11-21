f7dd3683ca42586769d85a79ca3ec5d6a72d5ef2
Vbobell <vinicius.bobel41@gmail.com>
2017-11-21 21:44:05 -0200

    Change manage admin methods

--- controller/manage/manageAdmin/manage-admin.js
+++ controller/manage/manageAdmin/manage-admin.js
@@ -2,31 +2,28 @@ const CrudAdmin = require('../../../models/crud/crudAdmin/crud-admin');
 
 class ManageAdmin{
     constructor(){
+        this.crudAdmin = new CrudAdmin();
     }
     getDataAdmins(parametres, callback){
-        let crudAdmin = new CrudAdmin();
-        crudAdmin.selectUsers(parametres.limit, parametres.offset , data =>{
+        this.crudAdmin.selectUsers(parametres.limit, parametres.offset , data =>{
             return callback(data);
         });
     }
     removeAdmin(parametres, callback){
-        let crudAdmin = new CrudAdmin();
         let where = "id_admin='"+parametres.idAdmin+"'";
-        crudAdmin.executeDelete('admin', where, data =>{
+        this.crudAdmin.executeDelete('admin', where, data =>{
             return callback(data);
         });
     }
     editAdmin(parametres, callback){
-        let crudAdmin = new CrudAdmin();
         let values = "('" + parametres.name + "', '" + parametres.registre + "')";
         let where = "id_admin='"+parametres.idAdmin+"'";
-        crudAdmin.executeUpdate('admin', '(name_admin, registre)', values, where, data => {
+        this.crudAdmin.executeUpdate('admin', '(name_admin, registre)', values, where, data => {
             return callback(data);
         });
     }
     loginValidation(parametres, callback){
-        let crudAdmin = new CrudAdmin();
-        crudAdmin.selectUser(parametres.user,parametres.password, data => {
+        this.crudAdmin.selectUser(parametres.user,parametres.password, data => {
             if(JSON.parse(data)[0].count == 1)
                 return callback(true);
             else
