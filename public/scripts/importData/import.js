90ab75adddab8e96bfd21fa96c7a856048ad8ca1
Vbobell <vinicius.bobel41@gmail.com>
2017-12-04 21:09:11 -0200

    Create generic structure js for import files

---
+++ public/scripts/importData/import.js
@@ -0,0 +1,29 @@
+(function(){
+    var form;
+    var upload = {
+        file : function(element, label){
+            element.change(function (event) {
+                form = new FormData();
+                form.append('csv', event.target.files[0]);
+                if(event.target.files[0].type != "application/vnd.ms-excel"){
+                    alert('Arquivo inválido');
+                }else{
+                    label.text(event.target.files[0].name);
+                }
+            });
+        },
+        uploadReady : function(elementSubmit, route){
+            elementSubmit.on('click', function(){
+                $.ajax({
+                    url : route,
+                    data: form,
+                    processData: false,
+                    contentType: false,
+                    type: 'POST'
+                }).done(function(data){
+                    alert("Arquivo importado com sucesso!");
+                });
+            });
+        }
+    }
+})();
