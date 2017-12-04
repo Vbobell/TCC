f681da3191e4490f46288b8a842d24c18a68a7d9
Vbobell <vinicius.bobel41@gmail.com>
2017-12-04 20:45:52 -0200

    create sql for table discipline

--- models/base.sql
+++ models/base.sql
@@ -16,12 +16,25 @@ create table class_(
     name_class varchar(30) NOT NULL
 );
 
+create table discipline(
+    id_discipline serial NOT NULL primary key,
+    name_discipline varchar(30) NOT NULL
+);
+
 create table course_class(
     id_course_class serial NOT NULL primary key,
     id_course int NOT NULL,
     id_class int NOT NULL,
     foreign key (id_course) references course (id_course),
     foreign key (id_class) references class_ (id_class)
+);
+
+create course_discipline(
+    id_course_discipline serial NOT NULL primary key,
+    id_course int NOT NULL,
+    id_discipline int NOT NULL,
+    foreign key (id_course) references course (id_course),
+    foreign key (id_discipline) references discipline (id_discipline)
 );
 
 create table teacher(
