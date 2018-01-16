fe81e8dea98beed430f7abf426a82cab17fabf77
Vbobell <vinicius.bobel41@gmail.com>
2018-01-15 23:04:08 -0200

    change query attributes

--- models/crud/crudCourseDiscipline/crud-course-discipline.js
+++ models/crud/crudCourseDiscipline/crud-course-discipline.js
@@ -7,7 +7,8 @@ class CrudCourseDiscipline extends Crud{
                 if (err) {
                     return console.error('error fetching client from pool', err);
                 }
-                client.query(`SELECT id_course, name_course, id_discipline, name_discipline 
+                client.query(`SELECT course.id_course, course.name_course, 
+                            course_discipline.id_discipline, course_discipline.name_discipline 
                             from course, discipline, course_discipline 
                             where course_discipline.id_course = course.id_course and 
                             discipline.id_discipline = course_discipline.id_discipline 
