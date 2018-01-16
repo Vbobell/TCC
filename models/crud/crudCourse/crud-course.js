79d8d236120793bef85bb0bb3ac6126433facdc6
Vbobell <vinicius.bobel41@gmail.com>
2018-01-15 22:03:51 -0200

    create query for search course and discipline

--- models/crud/crudCourse/crud-course.js
+++ models/crud/crudCourse/crud-course.js
@@ -1,13 +1,17 @@
 const Crud = require('../generic-crud');
 
 class CrudCourse extends Crud{
-    selectCourses(limit, offset, json){
+    selectCoursesAndDiscipline(limit, offset, json){
         this.getPool((data) =>{
             data.connect(function (err, client, done) {
                 if (err) {
                     return console.error('error fetching client from pool', err);
                 }
-                client.query('SELECT id_course, name_course from course limit $1 offset $2;', [limit, offset] , function (err, result) {
+                client.query(`SELECT id_course, name_course, id_discipline, name_discipline 
+                            from course, discipline, course_discipline 
+                            where course_discipline.id_course = course.id_course and 
+                            discipline.id_discipline = course_discipline.id_discipline 
+                            limit $1 offset $2;`, [limit, offset] , function (err, result) {
                     done();
                     if (err) {
                         return console.error('error running query', err);
