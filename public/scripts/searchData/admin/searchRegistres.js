1440ac46a7fef5ca0e6f60867badebaac5c53091
Vbobell <vinicius.bobel41@gmail.com>
2018-01-22 22:30:18 -0200

    create method get data registre

--- public/scripts/searchData/admin/searchRegistres.js
+++ public/scripts/searchData/admin/searchRegistres.js
@@ -1,12 +1,10 @@
-(function () {
-    var getGenericData = function(parametres, callback){
-        $.ajax({
-            url: '/admin/getData',
-            type: 'GET',
-            data: parametres,
-            async : true
-        }).done(function(data){
-            callback(data);
-        });
-    };
-})();
+var getGenericData = function(parametres, callback){
+    $.ajax({
+        url: '/admin/getData',
+        type: 'GET',
+        data: parametres,
+        async : true
+    }).done(function(data){
+        callback(data);
+    });
+};
