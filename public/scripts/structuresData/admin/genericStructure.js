380c09b30e4c23afca91339017666ed43e281127
Vbobell <vinicius.bobel41@gmail.com>
2018-01-23 23:05:44 -0200

    create generic structure

---
+++ public/scripts/structuresData/admin/genericStructure.js
@@ -0,0 +1,8 @@
+var list = function(registres, callback){
+    var structure = '<ul>';
+    registres.forEach(function(item, i){
+        structure += `<li> ${ item } </li>`; 
+    });
+    structure += '<ul>';
+    callback(structure);
+};
