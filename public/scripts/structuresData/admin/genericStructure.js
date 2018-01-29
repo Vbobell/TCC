01e7f841417a59c34f4cf5b6c73c55c9070f0089
Vbobell <vinicius.bobel41@gmail.com>
2018-01-29 21:54:02 -0200

    create generic structure for list disciplines

--- public/scripts/structuresData/admin/genericStructure.js
+++ public/scripts/structuresData/admin/genericStructure.js
@@ -6,3 +6,13 @@ var list = function(registres, callback){
     structure += '</ul>';
     callback(structure);
 };
+
+var listInputCheckBox = function(registres, callback){
+    var structure = '<ul>';
+    registres.forEach(function(item, i){
+        structure += `<li><input name="${item.name + item.id}" type="checkbox"/> 
+                     <label for="${item.name + item.id}">${ item.value }</label> </li>`; 
+    });
+    structure += '</ul>';
+    callback(structure);
+};
