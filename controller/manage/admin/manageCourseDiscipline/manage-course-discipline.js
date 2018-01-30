48fade3ba0da0c9636f28a18d8b4809b8fcbc35e
Vbobell <vinicius.bobel41@gmail.com>
2018-01-29 23:25:46 -0200

    create structure insert registres

--- controller/manage/admin/manageCourseDiscipline/manage-course-discipline.js
+++ controller/manage/admin/manageCourseDiscipline/manage-course-discipline.js
@@ -3,11 +3,16 @@ const CrudCourseDiscipline = require('../../../../models/crud/crudCourseDiscipli
 class ManageCourseDiscipline{
     constructor(){
         this.crudCourseDiscipline = new CrudCourseDiscipline();
+        this.data = ''; 
     }
     getDataCourseDiscipline(parametres, callback){
         this.crudCourseDiscipline.selectCoursesAndDiscipline(parametres.limit, parametres.offset , data =>{
             return callback(data);
         });
+    }
+    insertDataCourseDiscipline(data, callback) {
+        this.crudCourseDiscipline.executeInsert('course_discipline','(id_course, id_discipline)', '($1, $2)', 
+            data, response => callback(response));
     }
 }
 
