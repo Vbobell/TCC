6b3486a4ba6071be06668b33575df76fa9edfc10
Vbobell <vinicius.bobel41@gmail.com>
2018-03-12 22:45:59 -0300

    Change insert reward

--- models/insert.sql
+++ models/insert.sql
@@ -1,41 +1,41 @@
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('primeira atividade', 'first-activity');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('primeira atividade', 'primeira atividade', 'first-activity');
 
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('primeira atividade correta', 'first-correct-activity');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('primeira atividade correta', 'primeira atividade correta', 'first-correct-activity');
 
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('primeiro(a) a realizar a atividade', 'first-do-activity');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('primeiro(a) a realizar a atividade', 'primeiro(a) a realizar a atividade', 'first-do-activity');
 
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('sequência de atividades realizadas corretas', 'sequence-activity');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('sequência de atividades realizadas corretas', 'sequência de atividades realizadas corretas', 'sequence-activity');
 
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('atividade realizada', 'correct-activity');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('atividade realizada', 'atividade realizada', 'correct-activity');
 
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('atividade realizada ouro', 'gold-activity');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('atividade realizada ouro','atividade realizada ouro', 'gold-activity');
 
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('atividade realizada prata', 'silver-activity');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('atividade realizada prata', 'atividade realizada prata', 'silver-activity');
 
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('atividade realizada bronze', 'bronze-activity');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('atividade realizada bronze', 'atividade realizada bronze', 'bronze-activity');
 
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('primeira atividade ouro realizada', 'first-activity-gold');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('primeira atividade ouro realizada', 'primeira atividade ouro realizada', 'first-activity-gold');
 
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('primeira atividade prata realizada', 'first-activity-silver');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('primeira atividade prata realizada', 'primeira atividade prata realizada',  'first-activity-silver');
 
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('primeira atividade bronze realizada.', 'first-activity-bronze');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('primeira atividade bronze realizada.', 'primeira atividade bronze realizada.', 'first-activity-bronze');
 
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('sequência de atividades ouro realizas corretas', 'gold-sequence-activity');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('sequência de atividades ouro realizas corretas', 'sequência de atividades ouro realizas corretas', 'gold-sequence-activity');
 
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('sequência de atividades prata realizas corretas', 'silver-sequence-activity');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('sequência de atividades prata realizas corretas', 'sequência de atividades prata realizas corretas', 'silver-sequence-activity');
 
-INSERT INTO reward (name_reward, file_reward)
-VALUES ('sequência de atividades bronze realizas corretas', 'bronze-sequence-activity');
+INSERT INTO reward (name_reward, description_reward, file_reward)
+VALUES ('sequência de atividades bronze realizas corretas', 'sequência de atividades bronze realizas corretas', 'bronze-sequence-activity');
